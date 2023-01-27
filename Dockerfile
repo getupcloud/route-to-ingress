21142a7f9ebd1bdd161f686c4c9f719f1306c330
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2023-01-27 14:08:54 -0300

    Add commandline params

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM python:3.10
 RUN pip install kubernetes
 ADD . /app
-CMD python3 /app/main.py
+WORKDIR /app
+CMD python3 /app/app.py
--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 # param retry started runtime logger memo resource patch body reason diff old new spec meta status uid name namespace labels annotations
 #
 
+import argparse
 import json
 import logging
 import os
@@ -13,27 +14,27 @@ import sys
 import time
 from queue import Queue
 from threading import Event, Thread
-
 import kubernetes as kube
 
-# from openshift.dynamic import DynamicClient
-PID = os.getpid()
-logging.basicConfig(
-    #format=f'time="%(asctime)s" pid=%(process)d file=%(funcName)s:%(lineno)d level=%(levelname)s message="%(message)s"',
-    format=f'time="%(asctime)s" level=%(levelname)s message="%(message)s"',
-    level=logging.INFO,
-)
 
 INFO = logging.info
 DEBUG = logging.debug
 ERROR = logging.error
 
-
-def JSON(d):
-    try:
-        return json.dumps(dict(d), indent=3)
-    except:
-        return str(d)
+DEFAULT_NAMESPACES = [ n.strip() for n in os.environ.get('NAMESPACES', '').split(',') ]
+DEFAULT_IGNORED_NAMESPACES = [ n.strip() for n in os.environ.get('IGNORED_NAMESPACES', ','.join([
+    'openshift-adp',
+    'openshift-authentication',
+    'openshift-console',
+    'openshift-image-registry',
+    'openshift-ingress-canary'
+])).split(',') ]
+DEFAULT_CLUSTER_DOMAIN = os.environ.get("CLUSTER_DOMAIN", "")
+DEFAULT_ALLOWED_DOMAINS = os.environ.get("ALLOWED_DOMAINS", "")
+DEFAULT_INGRESS_CONFIG_NAME = os.environ.get("INGRESS_CONFIG_NAME", "cluster")
+DEFAULT_CLUSTER_ISSUER = os.environ.get("CLUSTER_ISSUER", "letsencrypt-staging-http01")
+DEFAULT_INGRESS_CLASS_NAME = os.environ.get("INGRESS_CLASS_NAME", "route-to-ingress")
+DEFAULT_IGNORE_DANGEROUS_INGRESS_CLASS_NAME = os.environ.get("IGNORE_DANGEROUS_INGRESS_CLASS_NAME", "false")
 
 
 GROUP_ROUTE, GROUP_ROUTE_VERSION, GROUP_ROUTE_PLURAL = (
@@ -41,18 +42,14 @@ GROUP_ROUTE, GROUP_ROUTE_VERSION, GROUP_ROUTE_PLURAL = (
     "v1",
     "routes",
 )
-GROUP_CONFIG, GROUP_CONFIG_VERSION, GROUP_CONFIG_PLURAL = (
+
+GROUP_CONFIG, GROUP_CONFIG_VERSION = (
     "config.openshift.io",
-    "v1",
-    "ingresses",
+    "v1"
 )
+GROUP_CONFIG_INGRESS_PLURAL = "ingresses"
 
 VALID_TLS_TERMINATIONS = ("edge", "reencrypt")
-
-NAMESPACE = os.environ.get('NAMESPACE')
-DRY_RUN = (os.environ.get('DRY_RUN', 'false') == 'true')
-if DRY_RUN:
-    INFO('DRY_RUN is active')
 
 ANNOTATION_TLS_PREFIX = "tls.getup.io"
 # Ignore Routes with this annotation.
@@ -74,26 +71,73 @@ ANNOTATIONS_CERT_UTILS_OPERATOR = "cert-utils-operator.redhat-cop.io"
 # Cert Utils Operator source secret annotation
 ANNOTATIONS_CERT_UTILS_OPERATOR_CERTS_FROM_SECRET = f"{ANNOTATIONS_CERT_UTILS_OPERATOR}/certs-from-secret"
 
-# Cluster apps domain. Defaults to discovered from object below.
-ENV_CLUSTER_DOMAIN = os.environ.get("CLUSTER_DOMAIN")
-# Name of ingresses.config.openshift.io to look for ingress domain suffix
-ENV_INGRESS_CONFIG_NAME = os.environ.get("INGRESS_CONFIG_NAME", "cluster")
-# Default ClusterIssuer name to set into created Ingress
-ENV_CLUSTER_ISSUER = os.environ.get("CLUSTER_ISSUER", "letsencrypt-staging-http01")
-# Default Ingress Class name to set into created Ingress
-# Do not set this to 'openshift-default' in order to avoid an infinite loop
-# creating ingress->route->ingress->route...
-ENV_INGRESS_CLASS_NAME = os.environ.get("INGRESS_CLASS_NAME", "route-to-ingress")
-ENV_IGNORE_DANGEROUS_INGRESS_CLASS_NAME = os.environ.get("IGNORE_DANGEROUS_INGRESS_CLASS_NAME", "false")
+args = None
+parser = argparse.ArgumentParser(description='Controller to create Ingress objects from Route objects.', add_help=True)
 
-if (ENV_INGRESS_CLASS_NAME == "openshift-default" and ENV_IGNORE_DANGEROUS_INGRESS_CLASS_NAME != "true"):
-    ERROR("Setting INGRESS_CLASS_NAME=openshift-default may lead to infinite creation of ingresses/routes by openshift router controller.")
-    ERROR("Please also set IGNORE_DANGEROUS_INGRESS_CLASS_NAME=true if you known what you are doing.")
-    sys.exit(1)
+parser.add_argument('-v', '--verbose',
+    help='Turn on verbose logs.',
+    action=argparse.BooleanOptionalAction,
+    default=False)
+
+parser.add_argument('--dry-run',
+    help='Look into namespace only. May be used multiple times.',
+    action=argparse.BooleanOptionalAction,
+    default=False)
+
+parser.add_argument('-n', '--namespaces',
+    help='Look into specified namespaces only.',
+    type=str,
+    nargs='*',
+    action="extend")
+
+parser.add_argument('-i', '--ignored-namespaces',
+    help=f'Look into specified namespaces only. (default: {", ".join(DEFAULT_IGNORED_NAMESPACES)})',
+    type=str,
+    nargs='*',
+    action="extend",
+    default=DEFAULT_IGNORED_NAMESPACES)
+
+parser.add_argument('--cluster-domain',
+    help='Set cluster domain. Auto-dicovered if not specified.',
+    type=str,
+    nargs=1)
+
+parser.add_argument('--allowed-domains',
+    help='What domains will be processed from Routes. Any other will be ignored. Defaults to value from --cluster-domain or auto-dicovery.',
+    type=str,
+    nargs='*')
+
+parser.add_argument('--ingress-config-name',
+    help=f'Object name for {GROUP_CONFIG}.{GROUP_CONFIG_INGRESS_PLURAL} for cluster-domain auto-dicover.',
+    type=str,
+    default=DEFAULT_INGRESS_CONFIG_NAME)
+
+parser.add_argument('--cluster-issuer',
+    help='ClusterIssuer name to add into newly created Ingresses.',
+    type=str,
+    default=DEFAULT_CLUSTER_ISSUER)
+
+parser.add_argument('--ingress-class-name',
+    help='Ingress Class Name to set into into newly created Ingresses. Note it can\'t conflict with existing ingress class names.',
+    type=str,
+    default=DEFAULT_INGRESS_CLASS_NAME)
+
+parser.add_argument('--ignore-conflicting-ingress-class-name',
+    help='Only to make sure you undestand you can\'t set an already used ingress class name.',
+    action=argparse.BooleanOptionalAction,
+    default=False)
 
 
 ############################################
 ## Common utils
+
+
+def JSON(d):
+    try:
+        return json.dumps(dict(d), indent=3)
+    except:
+        return str(d)
+
 
 class Cache:
     INGRESS_CACHE = {}
@@ -132,12 +176,12 @@ def discover_ingress_domain(api_client):
         config = api.get_cluster_custom_object(
             GROUP_CONFIG,
             GROUP_CONFIG_VERSION,
-            GROUP_CONFIG_PLURAL,
-            ENV_INGRESS_CONFIG_NAME,
+            GROUP_CONFIG_INGRESS_PLURAL,
+            args.ingress_config_name,
         )
         return config["spec"]["domain"]
     except (KeyError, kube.client.exceptions.ApiException) as ex:
-        ERROR(f"Failed to query for cluster domain ({GROUP_CONFIG_VERSION}.{GROUP_CONFIG}/{ENV_INGRESS_CONFIG_NAME}): {ex.status} {ex.reason}")
+        ERROR(f"Failed to query for cluster domain ({GROUP_CONFIG_VERSION}.{GROUP_CONFIG}/{args.ingress_config_name}): {ex.status} {ex.reason}")
         ERROR("Please set INGRESS_CONFIG_NAME or CLUSTER_DOMAIN")
         sys.exit(1)
 
@@ -185,7 +229,7 @@ def patch_route(route):
 
     # TODO: remove unnecessary fields prior patching
     try:
-        if DRY_RUN:
+        if args.dry_run:
             INFO('DRY_RUN: api.patch_namespaced_custom_object('
                 f'{GROUP_ROUTE}, '
                 f'{GROUP_ROUTE_VERSION}, '
@@ -289,10 +333,10 @@ def make_ingress(route):
     service_name = spec["to"]["name"]
     ingress_id = make_ingress_id()
 
-    INFO(f"make_ingress(api_version={api_version}, host={host}, path={path}, service_name={service_name}, service_port={service_port}, has_tls={has_tls}, ingress_id={ingress_id})")
+    INFO(f"make_ingress(api_version={api_version}, host={host}, path={path}, service_name={service_name}, service_port={service_port}, ingress_id={ingress_id})")
 
     anns = {
-        ANNOTATION_CERT_MANAGER_CLUSTER_ISSUER: ENV_CLUSTER_ISSUER,
+        ANNOTATION_CERT_MANAGER_CLUSTER_ISSUER: args.cluster_issuer,
         ANNOTATION_TLS_SOURCE_ROUTE: name,
         ANNOTATION_TLS_INGRESS_ID: ingress_id,
     }
@@ -303,7 +347,7 @@ def make_ingress(route):
             INFO(f"Discarding annotation: {k}")
 
     secret_name = anns.get(ANNOTATION_TLS_INGRESS_SECRET_NAME, f'{name}-ingress-tls')
-    ingress_class_name = anns.pop("kubernetes.io/ingress.class", ENV_INGRESS_CLASS_NAME)
+    ingress_class_name = anns.pop("kubernetes.io/ingress.class", args.ingress_class_name)
 
     return {
         "metadata": {
@@ -344,7 +388,7 @@ def make_ingress(route):
             ],
             "tls": [
                 {
-                    "hosts": [f"{ingress_id}.{ENV_CLUSTER_DOMAIN}", host],
+                    "hosts": [f"{ingress_id}.{args.cluster_domain}", host],
                     "secretName": secret_name,
                 }
             ]
@@ -356,7 +400,6 @@ def create_ingress(route):
     meta, spec = route['metadata'], route['spec']
     namespace, name = meta["namespace"], meta["name"]
 
-    INFO(f"New Ingress ID: {ingress_id}")
     ingress = make_ingress(route)
 
     if not ingress:
@@ -368,7 +411,7 @@ def create_ingress(route):
 
     api = kube.client.NetworkingV1Api()
     try:
-        if DRY_RUN:
+        if args.dry_run:
             INFO(f'DRY_RUN: api.create_namespaced_ingress(namespace={namespace}, name={name}, body=ingress)')
         else:
             api.create_namespaced_ingress(namespace=namespace, body=ingress)
@@ -382,7 +425,7 @@ def patch_ingress(namespace, name, ingress):
     api = kube.client.NetworkingV1Api()
     # TODO: remove unnecessary fields prior patching
     try:
-        if DRY_RUN:
+        if args.dry_run:
             INFO('DRY_RUN: api.patch_namespaced_ingress('
                 f'namespace={namespace}, '
                 f'name={name}, '
@@ -401,10 +444,10 @@ def ensure_ingress(ingress):
     meta, spec = ingress['metadata'], ingress['spec']
     changed = False
 
-    if ingress['metadata'].get('annotations', {}).get(ANNOTATION_CERT_MANAGER_CLUSTER_ISSUER) != ENV_CLUSTER_ISSUER:
+    if ingress['metadata'].get('annotations', {}).get(ANNOTATION_CERT_MANAGER_CLUSTER_ISSUER) != args.cluster_issuer:
         if 'annotations' not in ingress['metadata']:
             ingress['metadata']['annotations'] = {}
-        ingress['metadata']['annotations'][ANNOTATION_CERT_MANAGER_CLUSTER_ISSUER] = ENV_CLUSTER_ISSUER
+        ingress['metadata']['annotations'][ANNOTATION_CERT_MANAGER_CLUSTER_ISSUER] = args.cluster_issuer
         changed = True
 
     if changed:
@@ -418,7 +461,7 @@ def delete_ingress(ingress):
 
     INFO(f'Deleting ingress: {namespace}/{name}')
     try:
-        if DRY_RUN:
+        if args.dry_run:
             INFO(f'DRY_RUN: api.delete_namespaced_ingress(namespace={namespace}, name={name})')
         else:
             api.delete_namespaced_ingress(namespace=namespace, name=name)
@@ -438,7 +481,7 @@ def handle_ingress_create(meta, spec, ingress):
 def handle_ingress_update(meta, spec, ingress):
     CACHE.set(ingress)
 
-    ensure_ingress(meta, spec, ingress)
+    ensure_ingress(ingress)
 
 
 def handle_ingress_delete(meta, spec, ingress):
@@ -450,11 +493,10 @@ def handle_ingress_delete(meta, spec, ingress):
 
 # TODO: unify both informers
 class RouteInformer(Thread):
-    def __init__(self, api_client, queue, namespace, stop_event):
+    def __init__(self, api_client, queue, stop_event):
         Thread.__init__(self, name="RouteInformer")
         self.api_instance = kube.client.CustomObjectsApi(api_client)
         self.queue = queue
-        self.namespace = namespace
         self.stop_event = stop_event
 
     def run(self):
@@ -465,23 +507,13 @@ class RouteInformer(Thread):
                 DEBUG("watch route")
                 try:
                     self.watcher = kube.watch.Watch()
-                    if self.namespace:
-                        self.stream = self.watcher.stream(
-                            self.api_instance.list_namespaced_custom_object,
-                            GROUP_ROUTE,
-                            GROUP_ROUTE_VERSION,
-                            self.namespace,
-                            GROUP_ROUTE_PLURAL,
-                            resource_version=resource_version,
-                            timeout_seconds=0)
-                    else:
-                        self.stream = self.watcher.stream(
-                            self.api_instance.list_cluster_custom_object,
-                            GROUP_ROUTE,
-                            GROUP_ROUTE_VERSION,
-                            GROUP_ROUTE_PLURAL,
-                            resource_version=resource_version,
-                            timeout_seconds=0)
+                    self.stream = self.watcher.stream(
+                        self.api_instance.list_cluster_custom_object,
+                        GROUP_ROUTE,
+                        GROUP_ROUTE_VERSION,
+                        GROUP_ROUTE_PLURAL,
+                        resource_version=resource_version,
+                        timeout_seconds=0)
                     for event in self.stream:
                         DEBUG(JSON(event))
                         self.queue.put(event)
@@ -490,11 +522,10 @@ class RouteInformer(Thread):
 
 
 class IngressInformer(Thread):
-    def __init__(self, api_client, queue, namespace, stop_event):
+    def __init__(self, api_client, queue, stop_event):
         Thread.__init__(self, name="IngressInformer")
         self.api_instance = kube.client.NetworkingV1Api(api_client)
         self.queue = queue
-        self.namespace = namespace
         self.stop_event = stop_event
 
     def run(self):
@@ -505,17 +536,10 @@ class IngressInformer(Thread):
                 DEBUG("watch ingress")
                 try:
                     self.watcher = kube.watch.Watch()
-                    if self.namespace:
-                        self.stream = self.watcher.stream(
-                            self.api_instance.list_namespaced_ingress,
-                            self.namespace,
-                            resource_version=resource_version,
-                            timeout_seconds=0)
-                    else:
-                        self.stream = self.watcher.stream(
-                            self.api_instance.list_ingress_for_all_namespaces,
-                            resource_version=resource_version,
-                            timeout_seconds=0)
+                    self.stream = self.watcher.stream(
+                        self.api_instance.list_ingress_for_all_namespaces,
+                        resource_version=resource_version,
+                        timeout_seconds=0)
                     for event in self.stream:
                         DEBUG(JSON(event))
                         self.queue.put(event)
@@ -545,6 +569,25 @@ def force_exit(signum, frame):
 
 
 if __name__ == "__main__":
+    args = parser.parse_args()
+
+    if args.verbose:
+        log_level = logging.DEBUG
+        log_fmt = f'time="%(asctime)s" pid=%(process)d file=%(funcName)s:%(lineno)d level=%(levelname)s message="%(message)s"'
+    else:
+        log_level = logging.INFO
+        log_fmt = f'time="%(asctime)s" level=%(levelname)s message="%(message)s"'
+
+    logging.basicConfig(format=log_fmt, level=log_level)
+
+    if args.dry_run:
+        INFO('Dry-run is active')
+
+    if (args.ingress_class_name != DEFAULT_INGRESS_CLASS_NAME and not args.ignore_conflicting_ingress_class_name):
+        ERROR("Setting --ingress-class-name may lead to infinite creation of ingresses/routes by openshift router controller.")
+        ERROR("Please also set --ignore-conflicting-ingress-class-name if you known what you are doing.")
+        sys.exit(1)
+
     signal.signal(signal.SIGINT, sig_handler)
     signal.signal(signal.SIGTERM, sig_handler)
     signal.signal(signal.SIGALRM, force_exit)
@@ -558,21 +601,21 @@ if __name__ == "__main__":
 
     api_client = kube.client.api_client.ApiClient()
 
-    if not ENV_CLUSTER_DOMAIN:
-        ENV_CLUSTER_DOMAIN = discover_ingress_domain(api_client)
-        INFO(f"Found Ingress domain: {ENV_INGRESS_CLASS_NAME}")
+    if not args.cluster_domain:
+        args.cluster_domain = discover_ingress_domain(api_client)
+        INFO(f"Found Ingress domain: {args.ingress_class_name}")
 
     queue = Queue()
     stop_event = Event()
 
-    ingress_informer = IngressInformer(api_client, queue, NAMESPACE, stop_event)
+    ingress_informer = IngressInformer(api_client, queue, stop_event)
     ingress_informer.daemon = True
     ingress_informer.start()
 
     # give some time for Ingress to fill cache
     time.sleep(2)
 
-    route_informer = RouteInformer(api_client, queue, NAMESPACE, stop_event)
+    route_informer = RouteInformer(api_client, queue, stop_event)
     route_informer.daemon = True
     route_informer.start()
 
@@ -596,6 +639,12 @@ if __name__ == "__main__":
         kind = body['kind']
         meta = body['metadata']
         spec = body['spec']
+
+        if (meta['namespace'] in args.ignored_namespaces) or (args.namespaces and meta['namespace'] not in args.namespaces):
+            if meta['namespace'] not in args.ignored_namespaces:
+                INFO(f'Ignoring namespace {meta["namespace"]}')
+            args.ignored_namespaces.append(meta['namespace'])
+            continue
 
         if not spec:
             continue
